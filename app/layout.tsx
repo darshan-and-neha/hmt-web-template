d25e5e8db56a05ed49bf030945f152890612338e
Neha Rajesh <leehanseok3@gmail.com>
2024-01-18 02:43:22 +0000

    update nextjs setup config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,14 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css'
+
+import { ColorSchemeScript, MantineProvider } from '@mantine/core'
+
+export const metadata = {
+  title: 'My Mantine app',
+  description: 'I have followed setup instructions carefully',
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   )
 }
